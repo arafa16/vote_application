78629837047fe99818d6ac69e36b0f0747fa2862
ara fa adri <arafa.sco@gmail.com>
2026-07-07 17:19:24 +0700

    build: make company controller and route

---
+++ backend/src/controllers/company.controller.js
@@ -0,0 +1,179 @@
+const { company: companyModel } = require("../models/index.js");
+const { Op } = require("sequelize");
+const path = require("path");
+const crypto = require("crypto");
+const fs = require("fs");
+const CustomHttpError = require("../utils/custom_http_error.js");
+
+const getDatas = async (req, res) => {
+  const { uuid, name, sort } = req.query;
+
+  const where = {};
+  let order = [];
+
+  if (sort) {
+    const direction = sort.startsWith("-") ? "DESC" : "ASC";
+    const columnName = sort.replace(/^-/, "");
+    order.push([columnName, direction]);
+  }
+
+  if (name) {
+    where.name = {
+      [Op.like]: `%${name}%`,
+    };
+  }
+
+  const findDatas = await companyModel.findAll({ where });
+
+  return res.status(200).json({
+    success: true,
+    message: "success",
+    data: findDatas,
+  });
+};
+
+const getDataTable = async (req, res) => {
+  const { search, is_active, sort } = req.query;
+
+  let whereClause = {};
+  let order = [];
+
+  if (sort) {
+    const direction = sort.startsWith("-") ? "DESC" : "ASC";
+    const columnName = sort.replace(/^-/, "");
+    order.push([columnName, direction]);
+  }
+
+  if (search) {
+    whereClause = {
+      ...whereClause,
+      [Op.or]: [{ name: { [Op.like]: `%${search}%` } }],
+    };
+  }
+
+  if (is_active) {
+    whereClause.is_active = is_active;
+  }
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const offset = (page - 1) * limit;
+  const { rows, count } = await companyModel.findAndCountAll({
+    where: whereClause,
+    limit,
+    offset,
+  });
+
+  const pages = Math.ceil(count / limit);
+
+  return res.status(200).json({
+    success: true,
+    message: "success",
+    data: rows,
+    meta: {
+      total: count,
+      page,
+      limit,
+      pages,
+    },
+  });
+};
+
+const getDataById = async (req, res) => {
+  const { uuid } = req.params;
+
+  const findData = await companyModel.findOne({
+    where: { uuid },
+  });
+
+  if (!findData) {
+    throw new CustomHttpError("data not found", 404);
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "success",
+    data: findData,
+  });
+};
+
+const createData = async (req, res) => {
+  const { name, sequence, code } = req.body;
+
+  if (!name) {
+    throw new CustomHttpError("name cannot be null", 400);
+  }
+
+  const newData = await companyModel.create({
+    name,
+    sequence,
+    code,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: "success",
+    data: newData,
+  });
+};
+
+const updateData = async (req, res) => {
+  const { uuid } = req.params;
+  const { name, sequence, code } = req.body;
+
+  const findData = await companyModel.findOne({
+    where: { uuid },
+  });
+
+  if (!findData) {
+    throw new CustomHttpError("data not found", 404);
+  }
+
+  await findData.update({
+    name,
+    name,
+    sequence,
+    code,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "data updated successfully",
+  });
+};
+
+const deleteData = async (req, res) => {
+  const { uuid } = req.params;
+  const { permanent } = req.query;
+
+  const findData = await companyModel.findOne({
+    where: { uuid },
+  });
+
+  if (!findData) {
+    throw new CustomHttpError("data not found", 404);
+  }
+
+  if (Boolean(permanent) === true) {
+    await findData.destroy();
+  } else {
+    await findData.update({
+      is_active: false,
+    });
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "data deleted successfully",
+  });
+};
+
+module.exports = {
+  getDatas,
+  getDataTable,
+  getDataById,
+  createData,
+  updateData,
+  deleteData,
+};
