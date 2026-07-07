11cdd683803fa7d0040aa116324c2ad74a11b8cb
ara fa adri <arafa.sco@gmail.com>
2026-07-07 17:19:05 +0700

    build: make status controller and route

---
+++ backend/src/controllers/status.controller.js
@@ -0,0 +1,197 @@
+const { status: statusModel } = require("../models/index.js");
+const { Op } = require("sequelize");
+const CustomHttpError = require("../utils/custom_http_error.js");
+
+const getDatas = async (req, res) => {
+  const { uuid, name, sequence, code, sort } = req.query;
+
+  const where = {};
+  let order = [];
+
+  if (sort) {
+    const direction = sort.startsWith("-") ? "DESC" : "ASC";
+    const columnName = sort.replace(/^-/, "");
+    order.push([columnName, direction]);
+  }
+
+  if (name) {
+    where.name = {
+      [Op.like]: `%${name}%`,
+    };
+  }
+
+  const findDatas = await statusModel.findAll({
+    where,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "success",
+    data: findDatas,
+  });
+};
+
+const getDataTable = async (req, res) => {
+  const { search, is_active, sort } = req.query;
+
+  let whereClause = {};
+  let order = [];
+
+  if (sort) {
+    const direction = sort.startsWith("-") ? "DESC" : "ASC";
+    const columnName = sort.replace(/^-/, "");
+    order.push([columnName, direction]);
+  }
+
+  if (search) {
+    whereClause = {
+      ...whereClause,
+      [Op.or]: [{ name: { [Op.like]: `%${search}%` } }],
+    };
+  }
+
+  if (is_active) {
+    whereClause.is_active = is_active;
+  }
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const offset = (page - 1) * limit;
+  const { rows, count } = await statusModel.findAndCountAll({
+    where: whereClause,
+    limit,
+    offset,
+  });
+
+  const pages = Math.ceil(count / limit);
+
+  return res.status(200).json({
+    success: true,
+    message: "Get status successfully",
+    data: rows,
+    meta: {
+      total: count,
+      page,
+      limit,
+      pages,
+    },
+  });
+};
+
+const getDataById = async (req, res) => {
+  const { uuid } = req.params;
+
+  const findData = await statusModel.findOne({
+    where: { uuid },
+  });
+
+  if (!findData) {
+    throw new CustomHttpError("data not found", 404);
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "success",
+    data: findData,
+  });
+};
+
+const createData = async (req, res) => {
+  const { name, sequence, code } = req.body;
+
+  if (!name || !sequence || !code) {
+    throw new CustomHttpError("name, sequence, and code are required", 400);
+  }
+
+  const newData = await statusModel.create({
+    name,
+    sequence,
+    code,
+    is_active: true,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: "new data created successfully",
+    data: newData,
+  });
+};
+
+const updateData = async (req, res) => {
+  const { uuid } = req.params;
+  const { name, sequence, code, is_active } = req.body;
+
+  const findData = await statusModel.findOne({
+    where: { uuid },
+  });
+
+  if (!findData) {
+    throw new CustomHttpError("data not found", 404);
+  }
+
+  await findData.update({
+    name,
+    sequence,
+    code,
+    is_active,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "data updated successfully",
+    data: findData,
+  });
+};
+
+const deleteData = async (req, res) => {
+  const { uuid } = req.params;
+  const { permanent } = req.query;
+
+  const findData = await statusModel.findOne({
+    where: { uuid },
+  });
+
+  if (!findData) {
+    throw new CustomHttpError("data not found", 404);
+  }
+
+  if (permanent) {
+    if (permanent === "1") {
+      await findData.destroy();
+
+      return res.status(200).json({
+        success: true,
+        message: "data deleted permanent successfully",
+      });
+    } else {
+      await findData.update({
+        is_active: false,
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "data deleted successfully",
+        data: findData,
+      });
+    }
+  }
+
+  await findData.update({
+    is_active: false,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "data deleted successfully",
+  });
+};
+
+module.exports = {
+  getDatas,
+  getDataTable,
+  getDataById,
+  createData,
+  updateData,
+  deleteData,
+};
