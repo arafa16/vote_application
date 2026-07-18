f42837a2f17025b16d7e5541fe38f83819beed87
ara fa adri <arafa.sco@gmail.com>
2026-07-18 23:36:59 +0700

    build: make stores

---
+++ frontend/src/stores/sideMenuSlice.ts
@@ -0,0 +1,147 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+import { icons } from "../base-components/Lucide";
+
+export interface Menu {
+  icon: keyof typeof icons;
+  title: string;
+  pathname?: string;
+  subMenu?: Menu[];
+  ignore?: boolean;
+  privilege?: string;
+}
+
+export interface SideMenuState {
+  menu: Array<Menu | string>;
+}
+
+const initialState: SideMenuState = {
+  menu: [
+    "START MENU",
+    {
+      icon: "FileText",
+      title: "Tata Cara Voting",
+      pathname: "/",
+      privilege: "tata_cara_voting",
+    },
+    {
+      icon: "Users",
+      title: "Profile Kandidat Pengawas",
+      pathname: "/commissioner_profile",
+      privilege: "profile_kandidat_pengawas",
+    },
+    {
+      icon: "Users",
+      title: "Profile Kandidat Pengurus",
+      pathname: "/director_profile",
+      privilege: "profile_kandidat_pengurus",
+    },
+    {
+      icon: "Edit",
+      title: "Mulai Voting",
+      pathname: "/commissioner_candidate",
+      privilege: "mulai_voting",
+    },
+    {
+      icon: "CheckCircle",
+      title: "Riwayat Voting",
+      pathname: "/history_vote",
+      privilege: "riwayat_voting",
+    },
+    {
+      icon: "Monitor",
+      title: "Dashboard",
+      pathname: "/dashboard",
+      privilege: "dashboard",
+    },
+    {
+      icon: "Clipboard",
+      title: "Status Voting Anggota",
+      pathname: "/vote_data",
+      privilege: "status_voting_anggota",
+    },
+    {
+      icon: "Users",
+      title: "Data User",
+      pathname: "/users",
+      privilege: "user_data",
+    },
+    {
+      icon: "Settings",
+      title: "Setting",
+      privilege: "setting",
+      subMenu: [
+        {
+          icon: "Settings",
+          title: "application",
+          pathname: "/application",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "voting period",
+          pathname: "/voting_period",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "audit log",
+          pathname: "/audit_log",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "commissioner candidate",
+          pathname: "/commissioner_candidate_setup",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "company",
+          pathname: "/company",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "director candidate",
+          pathname: "/director_candidate_setup",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "privilege",
+          pathname: "/privilege",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "slider",
+          pathname: "/slider",
+          privilege: "setting",
+        },
+        {
+          icon: "Settings",
+          title: "status",
+          pathname: "/status",
+          privilege: "setting",
+        },
+      ],
+    },
+    {
+      icon: "LogOut",
+      title: "Keluar",
+      pathname: "/cara_voting",
+      privilege: "setting",
+    },
+  ],
+};
+
+export const sideMenuSlice = createSlice({
+  name: "sideMenu",
+  initialState,
+  reducers: {},
+});
+
+export const selectSideMenu = (state: RootState) => state.sideMenu.menu;
+
+export default sideMenuSlice.reducer;
---
+++ frontend/src/stores/store.ts
@@ -0,0 +1,43 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import darkModeReducer from "./darkModeSlice";
+import colorSchemeReducer from "./colorSchemeSlice";
+import sideMenuReducer from "./sideMenuSlice";
+import AuthReducer from "./features/AuthSlice";
+import GetMeReducer from "./features/GetMeSlice";
+import UserReducer from "./features/UserSlice";
+import PrivilegeReducer from "./features/PrivilegeSlice";
+import DirectorCandidateReducer from "./features/DirectorCandidateSlice";
+import VotingPeriodReducer from "./features/VotingPeriodSlice";
+import StatusVotingReducer from "./features/StatusVotingSlice";
+import CommissionerCandidateReducer from "./features/CommissionerCandidateSlice";
+import CommissionerVoteReducer from "./features/CommissionerVoteSlice";
+import DirectorVoteReducer from "./features/DirectorVoteSlice";
+import SliderReducer from "./features/SliderSlice";
+
+export const store = configureStore({
+  reducer: {
+    darkMode: darkModeReducer,
+    colorScheme: colorSchemeReducer,
+    sideMenu: sideMenuReducer,
+    auth: AuthReducer,
+    getMe: GetMeReducer,
+    user: UserReducer,
+    privilege: PrivilegeReducer,
+    directorCandidate: DirectorCandidateReducer,
+    commissionerCandidate: CommissionerCandidateReducer,
+    commissionerVote: CommissionerVoteReducer,
+    directorVote: DirectorVoteReducer,
+    votingPeriod: VotingPeriodReducer,
+    statusVoting: StatusVotingReducer,
+    slider: SliderReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
