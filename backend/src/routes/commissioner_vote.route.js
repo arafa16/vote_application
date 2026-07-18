d13ebd3399fbd1d068c145857d79a8015828c91f
ara fa adri <arafa.sco@gmail.com>
2026-07-18 15:13:35 +0700

    build: make controller to commissioner vote by user

--- backend/src/routes/commissioner_vote.route.js
+++ backend/src/routes/commissioner_vote.route.js
@@ -6,16 +6,32 @@ const {
   updateData,
   deleteData,
   getDataTable,
+  getDataCommissionerByUserNPeriod,
 } = require("../controllers/commissioner_vote.controller");
 const { verifyToken } = require("../middleware/auth.middleware");
+const {
+  voteSecurity,
+  voteCommissionerCheckBeforeSubmit,
+} = require("../middleware/vote.middleware");
 
 const router = express.Router();
 
 router.get("/datas", verifyToken, getDatas);
 router.get("/table", verifyToken, getDataTable);
 router.get("/data/:uuid", verifyToken, getDataById);
-router.post("/data", verifyToken, createData);
+router.post(
+  "/data",
+  verifyToken,
+  voteCommissionerCheckBeforeSubmit,
+  createData,
+);
 router.patch("/data/:uuid", verifyToken, updateData);
 router.delete("/data/:uuid", verifyToken, deleteData);
+router.get(
+  "/data_user_period/:user_uuid/:voting_period_uuid",
+  verifyToken,
+  voteSecurity,
+  getDataCommissionerByUserNPeriod,
+);
 
 module.exports = router;
