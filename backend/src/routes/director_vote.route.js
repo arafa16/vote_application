7a8b06bef7908f850b3d72e30b0c60436ec3d512
ara fa adri <arafa.sco@gmail.com>
2026-07-18 15:14:42 +0700

    build: setup new function to director vote route

--- backend/src/routes/director_vote.route.js
+++ backend/src/routes/director_vote.route.js
@@ -6,16 +6,28 @@ const {
   updateData,
   deleteData,
   getDataTable,
+  getDataByUserNPeriod,
 } = require("../controllers/director_vote.controller");
 const { verifyToken } = require("../middleware/auth.middleware");
+
+const {
+  voteSecurity,
+  voteDirectorCheckBeforeSubmit,
+} = require("../middleware/vote.middleware");
 
 const router = express.Router();
 
 router.get("/datas", verifyToken, getDatas);
 router.get("/table", verifyToken, getDataTable);
 router.get("/data/:uuid", verifyToken, getDataById);
-router.post("/data", verifyToken, createData);
+router.post("/data", verifyToken, voteDirectorCheckBeforeSubmit, createData);
 router.patch("/data/:uuid", verifyToken, updateData);
 router.delete("/data/:uuid", verifyToken, deleteData);
+router.get(
+  "/data_user_period/:user_uuid/:voting_period_uuid",
+  verifyToken,
+  voteSecurity,
+  getDataByUserNPeriod,
+);
 
 module.exports = router;
