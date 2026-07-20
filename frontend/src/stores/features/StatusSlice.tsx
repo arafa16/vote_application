01d5d4551bb70ff2a7c3827090a4021fcaf66e54
ara fa adri <arafa.sco@gmail.com>
2026-07-20 13:19:14 +0700

    build: make status features

---
+++ frontend/src/stores/features/StatusSlice.tsx
@@ -0,0 +1,250 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface variabel {
+  data: any;
+  isError: boolean;
+  isSuccess: boolean;
+  isLoading: boolean;
+  isLoadingPatch: boolean;
+  message: string;
+  messagePatch: string;
+}
+
+const initialState: variabel = {
+  data: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  isLoadingPatch: false,
+  message: "",
+  messagePatch: "",
+};
+
+export const GetStatusDatas: any = createAsyncThunk(
+  "Status/GetStatusDatas",
+  async (datas: any, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        import.meta.env.VITE_REACT_APP_API_URL +
+          `/api/v1/status/datas?${datas.searchParams}`,
+        {
+          withCredentials: true, // Now this is was the missing piece in the client side
+        },
+      );
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response);
+      }
+    }
+  },
+);
+
+export const GetStatusTable: any = createAsyncThunk(
+  "Status/GetStatusTable",
+  async (datas: any, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        import.meta.env.VITE_REACT_APP_API_URL +
+          `/api/v1/status/table?${datas}`,
+        {
+          withCredentials: true, // Now this is was the missing piece in the client side
+        },
+      );
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response);
+      }
+    }
+  },
+);
+
+export const GetStatusById: any = createAsyncThunk(
+  "Status/GetStatusById",
+  async (uuid: string, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        import.meta.env.VITE_REACT_APP_API_URL + `/api/v1/status/data/${uuid}`,
+        {
+          withCredentials: true, // Now this is was the missing piece in the client side
+        },
+      );
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response);
+      }
+    }
+  },
+);
+
+export const UpdateStatusData: any = createAsyncThunk(
+  "Status/UpdateStatusData",
+  async (datas: any, thunkAPI) => {
+    try {
+      const response = await axios.patch(
+        import.meta.env.VITE_REACT_APP_API_URL +
+          `/api/v1/status/data/${datas.uuid}`,
+        datas.formData,
+        {
+          withCredentials: true, // Now this is was the missing piece in the client side
+        },
+      );
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response);
+      }
+    }
+  },
+);
+
+export const CreateStatusData: any = createAsyncThunk(
+  "Status/CreateStatusData",
+  async (datas: any, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_REACT_APP_API_URL + `/api/v1/status/data`,
+        datas.formData,
+        {
+          withCredentials: true, // Now this is was the missing piece in the client side
+        },
+      );
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response);
+      }
+    }
+  },
+);
+
+export const DeleteStatusData: any = createAsyncThunk(
+  "Status/DeleteStatusData",
+  async (datas: any, thunkAPI) => {
+    try {
+      const response = await axios.delete(
+        import.meta.env.VITE_REACT_APP_API_URL +
+          `/api/v1/status/data/${datas.uuid}?permanent=1`,
+        {
+          withCredentials: true, // Now this is was the missing piece in the client side
+        },
+      );
+
+      return response.data;
+    } catch (error: any) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response);
+      }
+    }
+  },
+);
+
+export const StatusSlice = createSlice({
+  name: "Status",
+  initialState,
+  reducers: {
+    resetStatus: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    //GetStatusDatas
+    builder.addCase(GetStatusDatas.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(GetStatusDatas.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.data = action.payload;
+    });
+    builder.addCase(GetStatusDatas.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    });
+
+    //GetStatusTable
+    builder.addCase(GetStatusTable.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(GetStatusTable.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.data = action.payload;
+    });
+    builder.addCase(GetStatusTable.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    });
+
+    //GetStatusById
+    builder.addCase(GetStatusById.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(GetStatusById.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.data = action.payload;
+    });
+    builder.addCase(GetStatusById.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    });
+
+    //UpdateStatusData
+    builder.addCase(UpdateStatusData.pending, (state) => {
+      state.isLoadingPatch = true;
+    });
+    builder.addCase(UpdateStatusData.fulfilled, (state, action) => {
+      state.isLoadingPatch = false;
+      state.isSuccess = true;
+      state.messagePatch = action.payload;
+    });
+    builder.addCase(UpdateStatusData.rejected, (state, action) => {
+      state.isLoadingPatch = false;
+      state.isError = true;
+      state.messagePatch = action.payload;
+    });
+
+    //CreateDataStatus
+    builder.addCase(CreateStatusData.pending, (state) => {
+      state.isLoadingPatch = true;
+    });
+    builder.addCase(CreateStatusData.fulfilled, (state, action) => {
+      state.isLoadingPatch = false;
+      state.isSuccess = true;
+      state.messagePatch = action.payload;
+    });
+    builder.addCase(CreateStatusData.rejected, (state, action) => {
+      state.isLoadingPatch = false;
+      state.isError = true;
+      state.messagePatch = action.payload;
+    });
+
+    //DeleteDataStatus
+    builder.addCase(DeleteStatusData.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(DeleteStatusData.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.message = action.payload;
+    });
+    builder.addCase(DeleteStatusData.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    });
+  },
+});
+
+export const { resetStatus } = StatusSlice.actions;
+export default StatusSlice.reducer;
