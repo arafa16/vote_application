a28bf72eba8f6e6c91988b65199116591af87944
ara fa adri <arafa.sco@gmail.com>
2026-07-26 21:40:44 +0700

    build: setup send email invitation and activation user route

--- backend/src/routes/mailing.route.js
+++ backend/src/routes/mailing.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { sendEmailReset } = require("../controllers/mailing.controller");
+const {
+  sendEmailInvitation,
+  activationUser,
+} = require("../controllers/mailing.controller");
 const { verifyToken } = require("../middleware/auth.middleware");
 
 const router = express.Router();
 
-router.get("/user_reset/:uuid", verifyToken, sendEmailReset);
+router.get("/invitation/:uuid", verifyToken, sendEmailInvitation);
+router.get("/activation/:token", activationUser);
 
 module.exports = router;
