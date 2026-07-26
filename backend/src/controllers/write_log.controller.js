15d8111d5ae8ae77d82a615d6207f80e02a2f308
ara fa adri <arafa.sco@gmail.com>
2026-07-26 21:37:51 +0700

    build: make create log with transaction

--- backend/src/controllers/write_log.controller.js
+++ backend/src/controllers/write_log.controller.js
@@ -5,14 +5,17 @@ const {
 const { Op, where } = require("sequelize");
 const CustomHttpError = require("../utils/custom_http_error.js");
 
-const createLogHandler = async (params) => {
+const createLogHandler = async (params, transaction = null) => {
   const { user_id, activity, description } = params;
   try {
-    const create_log = await auditLogModel.create({
-      user_id,
-      activity,
-      description,
-    });
+    const create_log = await auditLogModel.create(
+      {
+        user_id,
+        activity,
+        description,
+      },
+      { transaction },
+    );
 
     return create_log;
   } catch (error) {
