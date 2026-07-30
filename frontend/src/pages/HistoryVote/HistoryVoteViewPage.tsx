6b00933145fe4d0c680f61781963b41bd257bdf4
ara fa adri <arafa.sco@gmail.com>
2026-07-30 09:26:47 +0700

    fixs: hide voting period in the history page

--- frontend/src/pages/HistoryVote/HistoryVoteViewPage.tsx
+++ frontend/src/pages/HistoryVote/HistoryVoteViewPage.tsx
@@ -138,11 +138,11 @@ const HistoryVoteViewPage = () => {
           </div>
         </div>
         <div className="col-span-12 md:col-span-3 col-start-1 md:col-start-10 mb-4">
-          <VotingPeriodForm
+          {/* <VotingPeriodForm
             datas={votingPeriodDatas}
             dataSelect={votingPeriodSelected}
             handleChange={handleChangeVotingPeriod}
-          />
+          /> */}
         </div>
       </div>
       <div
